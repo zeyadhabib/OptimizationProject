f370d1c231cc0d2e17f6eceb3d3f34a5aba8e987
Eyad Hany <eyadhany1998@yahoo.com>
2021-01-12 20:15:05 +0200

    Add minimize file

--- Minimize.py
+++ Minimize.py
@@ -1,16 +1,30 @@
 import numpy as np
 from scipy.optimize import minimize
 import math
-size = 100
-block = []
+import torch
+from train import PerformanceMeasurementModel
+
+size = 10000
+block = 10
+
+
+def get_prediction(block_size, model_path, size_scale, time_scale, mem_scale):
+    my_model = PerformanceMeasurementModel(1, 2, size_scale)
+    my_model.load_state_dict(torch.load(model_path))
+    my_model = my_model.cuda()
+    pred = my_model(torch.cuda.FloatTensor(np.array([[block_size]]).reshape((-1, 1))))
+    pred[:, 0], pred[:, 1] = pred[:, 0] * time_scale, pred[:, 1] / mem_scale
+    res = pred.detach().cpu().numpy()
+    return res[0, 0], res[0, 1]
 
 def timeOf(x): #time of block x
-    #predict the time taken by block x
-    return
+    time, memory = get_prediction(x, "performanceModel_new.pth", 50e6, 0.05, 100)
+    return time
 
-def sizeOf(x): #size of block x
+def memoryOf(x): #size of block x
     #predict the memory needed by block x
-    return
+    time, memory = get_prediction(x, "performanceModel_new.pth", 50e6, 0.05, 100)
+    return memory
 
 def f(x): #f(x) = time
     return timeOf(x)*math.floor(size/x) + timeOf(size%x)
