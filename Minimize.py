5217e9511ea60cc927afe6cf573ca195825cede0
Eyad Hany <eyadhany1998@yahoo.com>
2021-01-13 19:51:10 +0200

    adding f_dash

--- Minimize.py
+++ Minimize.py
@@ -1,14 +1,44 @@
 import numpy as np
-from scipy.optimize import minimize
+import matplotlib.pyplot as plt
 import math
 import torch
 from train import PerformanceMeasurementModel
 from scipy.optimize import minimize_scalar
+from scipy.optimize import fsolve
 
 
-size = 10000
-block = 10
+size=0
 
+
+def get_NN_grad(block_size: int):
+    size_scale = 1e6
+    time_scale = 3e6
+    mem_scale = 24
+    model = PerformanceMeasurementModel(1, 2, size_scale)
+    model.load_state_dict(torch.load("performanceModel_new.pth"))
+    nn_input = torch.FloatTensor(np.array([block_size]).reshape((-1, 1)))
+    nn_input.requires_grad = True
+    opt = torch.optim.Adam(params=model.parameters())
+    pred = model(nn_input)
+    dummy_memory = torch.zeros_like(pred)
+    dummy_memory[:, 0] = pred[:, 0]
+    pred2 = torch.nn.MSELoss()(pred, dummy_memory)
+    pred2.backward()
+    memory_grad = copy.deepcopy(nn_input.grad)
+    nn_input.grad.zero_()
+    opt.zero_grad()
+    pred = model(nn_input)
+    dummy_time = torch.zeros_like(pred)
+    dummy_time[:, 1] = pred[:, 1]
+    pred1 = torch.nn.MSELoss()(pred, dummy_time)
+    pred1.backward()
+    time_grad = copy.deepcopy(nn_input.grad)
+
+    return time_grad.detach().numpy()[0, 0] * time_scale, memory_grad.detach().numpy()[0, 0] / mem_scale
+
+def f_dash(x):
+  t_dash,m_dash = get_NN_grad(x)
+  return t_dash * (size/x) + timeOf(x) * (-1*size/x**2) + m_dash
 
 def get_prediction(block_size, model_path, size_scale, time_scale, mem_scale):
     my_model = PerformanceMeasurementModel(1, 2, size_scale)
@@ -20,12 +50,12 @@ def get_prediction(block_size, model_path, size_scale, time_scale, mem_scale):
     return res[0, 0], res[0, 1]
 
 def timeOf(x): #time of block x
-    time, memory = get_prediction(x, "performanceModel_new.pth", 50e6, 0.05, 100)
+    time, memory = get_prediction(x, "performanceModel_new.pth", 1e6, 3e-3, 24)
     return time
 
 def memoryOf(x): #size of block x
     #predict the memory needed by block x
-    time, memory = get_prediction(x, "performanceModel_new.pth", 50e6, 0.05, 100)
+    time,memory = get_prediction(x, "performanceModel_new.pth", 1e6, 3e-3, 24)
     return memory
 
 def f(x): #f(x) = time
@@ -41,9 +71,15 @@ def weighting(x): #weighting method
 
 
 def main():
-    minimumBlock = minimize(weighting , block , method='SLSQP')
-    min = minimize_scalar(weighting, bounds=(1, size), method='bounded')
-    print(min)
+    arr = [100, 200, 500, 1000, 10000, 60000, 100000, 500000, 1000000, 10000000, 1000000000]
+    minimum = []
+    for i in range(len(arr)):
+        size = arr[i]
+        minimum.append(minimize_scalar(weighting, bounds=(1, size), method='bounded').x)
+    print(minimum)
+    plt.plot(arr, minimum)
+    solution = fsolve(f_dash, minimum[4])
+    print(solution)
 
 
 if __name__ == "__main__":
